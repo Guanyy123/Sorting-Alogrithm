421fbc803237902a5a30ba954b8a81404a6f805d
guanyy <guan.yy@foxmail.com>
2020-04-08 16:32:00 +0800

    basic sorting notes

---
+++ sort.py
@@ -0,0 +1,72 @@
+##冒泡排序
+def bubbleSort(arr):
+    n = len(arr)
+    if n < 1:
+        return arr
+    
+    for i in range(n):
+        for j in range(n-i-1):
+            if arr[j] > arr[j+1]:   
+              arr[j+1], arr[j] = arr[j], arr[j+1]
+    return arr
+
+#选择排序入队
+def selectSort(arr):
+    n = len(arr)
+    for i in range(n-1):
+        min = i
+        for j in range(i+1, n):
+            if arr[j] < arr[min]:
+                min = j
+        
+        if min != i:
+            arr[min], arr[i] = arr[i], arr[min]
+    return arr
+
+#插入排序入队
+def insertSort(arr):
+    n =  len(arr)
+    if n < 1:
+        return arr
+
+    for i in range(1, n):
+        key = arr[i]
+        j = i
+        while j > 0 and key < arr[j-1]:
+           arr[j] = arr[j-1]
+           j-=1
+        arr[j] = key        
+    return arr
+
+#快速排序 空间复杂度O(logN), 时间复杂度
+def partation(arr, left, right):
+    #将左边第一个数定为key
+    key = left
+    #只要left 和right 向中间移动没有相遇
+    while left<right:
+        while left < right and arr[right] >= key:
+            right-=1
+        while left < right and arr[left] <= key:
+            left+=1
+        arr[left], arr[right] = arr[right], arr[left]
+    arr[left], arr[key] = arr[key], arr[left]
+    return left
+    
+
+def quickSort(arr, left, right):
+    
+    if left > right:
+        return
+    mid = partation(arr, left, right)
+    quickSort(arr, left, mid-1)
+    quickSort(arr, mid+1, right)
+
+def quickSortMain(arr):
+   return quickSort(arr, 0, len(arr)-1)
+
+if __name__ == "__main__":
+    # arr = [3,5,9,2,7,1,8,4,6,7]
+    # print(quickSortMain(arr))
+    s = "管乐阳正在学习"
+    s = "管乐阳"
+    print(s)
